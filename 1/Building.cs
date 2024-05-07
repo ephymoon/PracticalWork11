b36a9e0474d726737fe3e7aad7ae597d4b7e82a3
xdmain <loxBwf@gmail.com>
2024-05-07 14:06:35 +1000

    Add project files.

---
+++ 1/Building.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _1
+{
+    public class Building
+    {
+        public string city;
+        public string address;
+        public double height;
+
+        public Building()
+        {
+            city = "Москва";
+            address = "Пушкина 52";
+            height = 20;
+        }
+
+        public Building(string city, string address, double height)
+        {
+            this.city = city;
+            this.address = address;
+            this.height = height;
+        }
+
+        public Building(Building p1)
+        {
+            this.height = p1.height;
+            this.city = p1.city;
+            this.address = p1.address;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Дом в " + city + ", по адресу " + address + ", высотой " + height + "м.");
+        }
+    }
+    public class Apartment:Building
+    {
+        public int rent;
+        public Apartment()
+        : base()
+        {
+            rent = 10000;
+        }
+        public Apartment(string city, string address, double height, int rent)
+        : base(city, address, height)
+        {
+            this.rent = rent;
+        }
+        public Apartment(Apartment x1)
+        : base(x1.city, x1.address, x1.height)
+        {
+            rent = x1.rent;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Жилой дом в " + city + ", по адресу " + address + ", высотой " + height + "м." + "Стоимость аренды в сутки: " + rent + " руб.");
+        }
+        public void Count()
+        {
+            Console.WriteLine("Введите сколько суток будете проживать: ");
+            int days = Convert.ToInt32(Console.ReadLine());
+            days *= rent;
+            Console.WriteLine("Общая сумма для аренды: " + days + "руб.");
+        }
+    }
+    public class Mall : Building
+    {
+        public string name;
+        public Mall()
+        : base()
+        {
+            name = "Неизвестен";
+        }
+        public Mall(string city, string address, double height, string name)
+        : base(city, address, height)
+        {
+            this.name = name;
+        }
+        public Mall(Mall x1)
+        : base(x1.city, x1.address, x1.height)
+        {
+            name = x1.name;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Торговый центр " + name + " в " + city + ", по адресу " + address + ", высотой " + height + "м.");
+        }
+        public void Ad()
+        {
+            Console.WriteLine("Летние скидки в торговом центре " + name + ", до 90%. Контактный адрес: +79827214724");
+        }
+    }
+    public class Office : Building
+    {
+        public string company;
+        public Office()
+        : base()
+        {
+            company = "Неизвестная";
+        }
+        public Office(string city, string address, double height, string company)
+        : base(city, address, height)
+        {
+            this.company = company;
+        }
+        public Office(Office x1)
+        : base(x1.city, x1.address, x1.height)
+        {
+            company = x1.company;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Офисное здание " + " в " + city + ", по адресу " + address + ", высотой " + height + "м." + " В здании располагается компания " + company + ".");
+        }
+        public void Work()
+        {
+            Console.WriteLine("Компания " + company + " приглашает на работу молодых специалистов в сфере IT. По вопросам трудоустройства обращайтесь к секретарю на 1м этаже.");
+        }
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        public static void Values(int choise, ref Building building, ref Apartment apartment, ref Mall mall, ref Office office)
+        {
+            if (choise != 1)
+            {
+                Console.WriteLine("Класс Building");
+                Console.WriteLine("Введите город, адрес, высоту.");
+                building.city = Console.ReadLine();
+                building.address = Console.ReadLine();
+                building.height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Класс Apartment");
+                Console.WriteLine("Введите город, адрес, высоту, арендную плату(по суточно).");
+                apartment.city = Console.ReadLine();
+                apartment.address = Console.ReadLine();
+                apartment.height = Convert.ToDouble(Console.ReadLine());
+                apartment.rent = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Класс Mall");
+                Console.WriteLine("Введите город, адрес, высоту, название тц.");
+                mall.city = Console.ReadLine();
+                mall.address = Console.ReadLine();
+                mall.height = Convert.ToDouble(Console.ReadLine());
+                mall.name = Console.ReadLine();
+                Console.WriteLine("Класс Office");
+                Console.WriteLine("Введите город, адрес, высоту, название компании.");
+                office.city = Console.ReadLine();
+                office.address = Console.ReadLine();
+                office.height = Convert.ToDouble(Console.ReadLine());
+                office.company = Console.ReadLine();
+
+            }
+            else Console.WriteLine("Значения заданы по умолчанию.");
+        }
+
+        public static void Cout(ref Building building, ref Apartment apartment, ref Mall mall, ref Office office)
+        {
+            Console.WriteLine("Класс Building.");
+            building.Print();
+            Console.WriteLine("\nКласс Apartment.");
+            apartment.Print();
+            Console.WriteLine("Метод для класса:");
+            apartment.Count();
+            Console.WriteLine("\nКласс Mall.");
+            mall.Print();
+            Console.WriteLine("Метод для класса:");
+            mall.Ad();
+            Console.WriteLine("\nКласс Office.");
+            office.Print();
+            Console.WriteLine("Метод для класса:");
+            office.Work();
+        }
+
+        static void Main(string[] args)
+        {
+            Building building = new Building();
+            Apartment apartment = new Apartment();
+            Mall mall = new Mall();
+            Office office = new Office();
+        test:
+            Console.Clear();
+            Console.Write("\n1.Назначение программы.\n2.Указания пользователю.\n3.Выбор: задавать значения по стандарту или самостоятельно\n4.Вывод всех объектов и методов\n0.Выход из программы.");
+            Console.Write("\nВыберите действие: ");
+
+            switch (Console.ReadLine())
+            {
+                case ("1"):
+                    Console.Clear();
+                    Console.WriteLine("Программа классов зданий.");
+                    Console.Write("Нажмите любую клавишу, чтобы продолжить...");
+                    Console.ReadKey();
+                    goto test;
+                case ("2"):
+                    Console.Clear();
+                    Console.WriteLine("Для работы программы просто выберите соответсвубщий пункт меню, а позже при необходимости введите соответствующие данные.");
+                    Console.Write("Нажмите любую клавишу, чтобы продолжить...");
+                    Console.ReadKey();
+                    goto test;
+                case ("3"):
+                    Console.Clear();
+                    Console.Write("Нажмите 1, чтобы задать значения по умолчанию. 2 чтобы задать значения самостоятельно.");
+                    int choise = Convert.ToInt32(Console.ReadLine());
+                    Values(choise, ref building, ref apartment, ref mall, ref office);
+                    goto test;
+                case ("4"):
+                    Console.Clear();
+                    Cout(ref building, ref apartment, ref mall, ref office);
+                    Console.Write("Нажмите любую клавишу, чтобы выйти из просмотра...");
+                    Console.ReadKey();
+                    goto test;
+                case ("0"):
+                    break;
+                default:
+                    Console.WriteLine("Несуществующий вариант, повторите попытку.");
+                    goto test;
+            }
+            Console.Write("Нажмите любую клавишу, чтобы завершить работу программы...");
+            Console.ReadKey();
+        }
+    }
+}
